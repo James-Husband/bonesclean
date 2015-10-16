b4a0a4a71d863674de98a33e1f23fb0681f28dcd
James Husband <home@Jamess-iMac.local>
2015-10-16 19:16:46 +0100

    initial commit

---
+++ functions.php
@@ -0,0 +1,154 @@
+<?php
+/*
+Author: James Husband
+URL: https://github.com/James-Husband/bonespickedclean
+
+Description: BonesPickedClean,a stripped version of Bones using custom sass, skeleton grid and grunt management.
+
+
+*/
+
+// LOAD BONES CORE (if you remove this, the theme will break)
+require_once( 'library/bones.php' );
+
+// CUSTOMIZE THE WORDPRESS ADMIN (off by default)
+// require_once( 'library/admin.php' );
+
+/*********************
+LAUNCH BONES
+*********************/
+
+function bones_ahoy() {
+
+  // let's get language support going, if you need it
+  load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
+
+  // launching operation cleanup
+  add_action( 'init', 'bones_head_cleanup' );
+  // A better title
+  add_filter( 'wp_title', 'rw_title', 10, 3 );
+  // remove WP version from RSS
+  add_filter( 'the_generator', 'bones_rss_version' );
+  // remove pesky injected css for recent comments widget
+  add_filter( 'wp_head', 'bones_remove_wp_widget_recent_comments_style', 1 );
+  // clean up comment styles in the head
+  add_action( 'wp_head', 'bones_remove_recent_comments_style', 1 );
+  // clean up gallery output in wp
+  add_filter( 'gallery_style', 'bones_gallery_style' );
+
+  // enqueue base scripts and styles
+  add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
+  // ie conditional wrapper
+
+  // launching this stuff after theme setup
+  bones_theme_support();
+
+  // adding sidebars to Wordpress (these are created in functions.php)
+  add_action( 'widgets_init', 'bones_register_sidebars' );
+
+  // cleaning up random code around images
+  add_filter( 'the_content', 'bones_filter_ptags_on_images' );
+  // cleaning up excerpt
+  add_filter( 'excerpt_more', 'bones_excerpt_more' );
+
+} /* end bones ahoy */
+
+// let's get this party started
+add_action( 'after_setup_theme', 'bones_ahoy' );
+
+
+/************* OEMBED SIZE OPTIONS *************/
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 680;
+}
+
+/************* THUMBNAIL SIZE OPTIONS *************/
+
+// Thumbnail sizes
+add_image_size( 'bones-thumb-600', 600, 150, true );
+add_image_size( 'bones-thumb-300', 300, 100, true );
+
+
+add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
+
+/*
+Adds the ability to use the dropdown menu to select
+the new images sizes you have just created from within the media manager
+*/
+
+function bones_custom_image_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'bones-thumb-600' => __('600px by 150px'),
+        'bones-thumb-300' => __('300px by 100px'),
+    ) );
+}
+
+
+/************* ACTIVE SIDEBARS ********************/
+
+// Sidebars & Widgetizes Areas
+function bones_register_sidebars() {
+	register_sidebar(array(
+		'id' => 'sidebar',
+		'name' => __( 'Sidebar ', 'bonestheme' ),
+		'description' => __( 'The primary sidebar.', 'bonestheme' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+
+} // don't remove this bracket!
+
+
+/************* COMMENT LAYOUT *********************/
+
+// Comment Layout
+function bones_comments( $comment, $args, $depth ) {
+   $GLOBALS['comment'] = $comment; ?>
+  <div id="comment-<?php comment_ID(); ?>" <?php comment_class('cf'); ?>>
+    <article  class="cf">
+      <header class="comment-author vcard">
+        <?php
+        /*
+          this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
+          echo get_avatar($comment,$size='32',$default='<path_to_url>' );
+        */
+        ?>
+        <?php // custom gravatar call ?>
+        <?php
+          // create variable
+          $bgauthemail = get_comment_author_email();
+        ?>
+        <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
+        <?php // end custom gravatar call ?>
+        <?php printf(__( '<cite class="fn">%1$s</cite> %2$s', 'bonestheme' ), get_comment_author_link(), edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ) ?>
+        <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
+
+      </header>
+      <?php if ($comment->comment_approved == '0') : ?>
+        <div class="alert alert-info">
+          <p><?php _e( 'Your comment is awaiting moderation.', 'bonestheme' ) ?></p>
+        </div>
+      <?php endif; ?>
+      <section class="comment_content cf">
+        <?php comment_text() ?>
+      </section>
+      <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+    </article>
+  <?php // </li> is added by WordPress automatically ?>
+<?php
+} // don't remove this bracket!
+
+
+if (! function_exists('dashboard_footer') ){
+function dashboard_footer () {
+echo 'Managed by
+<a href="http://www.makebelievemedia.co.uk">Make Believe Media</a>';
+}
+}
+add_filter('admin_footer_text', 'dashboard_footer');
+
+/* DON'T DELETE THIS CLOSING TAG */ ?>
---
+++ library/admin.php
@@ -0,0 +1,37 @@
+<?php
+/*
+
+	- removing some default WordPress dashboard widgets
+	- an example custom dashboard widget
+	- adding custom login css
+	- changing text in footer of admin
+
+*/
+
+/************* DASHBOARD WIDGETS *****************/
+
+// disable default dashboard widgets
+function disable_default_dashboard_widgets() {
+	global $wp_meta_boxes;
+	// unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);    // Right Now Widget
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);        // Activity Widget
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']); // Comments Widget
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);  // Incoming Links Widget
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);         // Plugins Widget
+
+	// unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);    // Quick Press Widget
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);     // Recent Drafts Widget
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);           //
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);         //
+
+	// remove plugin dashboard boxes
+	unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']);           // Yoast's SEO Plugin Widget
+	unset($wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard']);        // Gravity Forms Plugin Widget
+	unset($wp_meta_boxes['dashboard']['normal']['core']['bbp-dashboard-right-now']);   // bbPress Plugin Widget
+
+}
+
+// removing the dashboard widgets
+add_action( 'wp_dashboard_setup', 'disable_default_dashboard_widgets' );
+
+?>
